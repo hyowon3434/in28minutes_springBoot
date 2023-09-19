20b879dca3871c15424434376e67335c6d6a8123
hyowon3434 <hyowon3434@naver.com>
2023-09-19 20:25:08 +0900

    initialized commit

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebbapp/MyfirstwebbappApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.myfirstwebbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebbappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/springboot/myfirstwebbapp/MyfirstwebbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.myfirstwebbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
