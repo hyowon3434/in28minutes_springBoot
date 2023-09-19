58771e808dd43320ecdf55acd2a629955cb88a98
hyowon3434 <hyowon3434@naver.com>
2023-09-19 21:18:45 +0900

    Feat: sayhello.jsp 기능 구현

--- src/main/java/com/in28minutes/springboot/myfirstwebbapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebbapp/hello/SayHelloController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.springboot.myfirstwebbapp.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,6 +10,31 @@ public class SayHelloController {
     @RequestMapping("say-Hello")
     @ResponseBody
     public String sayHello(){
-        return "Hello! What are you learning today??";
+        return "Hello! What are you learning today?????";
+    }
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My first HTML page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+
+    // sayHello.jsp
+    // "say-hello-jsp" -> sayHello.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>My first HTMK page-JSP</title>
+    </head>
+
+    <body>
+        success redirection to sayhello.jsp -JSP
+    </body>
+</html>
