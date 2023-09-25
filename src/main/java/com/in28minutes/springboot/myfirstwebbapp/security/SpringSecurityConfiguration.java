23ab75b01cb4bb80191dda8cc62cb29bd59810e3
hyowon3434 <hyowon3434@naver.com>
2023-09-25 18:06:01 +0900

    fix: h2database 관련 spring security 설정 변경

--- src/main/java/com/in28minutes/springboot/myfirstwebbapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebbapp/security/SpringSecurityConfiguration.java
@@ -2,13 +2,19 @@ package com.in28minutes.springboot.myfirstwebbapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -38,5 +44,15 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 }
